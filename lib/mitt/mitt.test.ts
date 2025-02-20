65ff850782587926ac9f26ae75d752cf9b61e218
soetas <3228891558@qq.com>
2025-02-20 11:08:00 +0800

    feat: update file

--- lib/mitt/mitt.test.ts
+++ lib/mitt/mitt.test.ts
@@ -1,3 +1,10 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import mitt from '.'
 
+describe('mitt', ()=>{
+  it('should default export be a function', ()=>{
+    expect(mitt).to.be.a('function')
+  })
+
+
+})
